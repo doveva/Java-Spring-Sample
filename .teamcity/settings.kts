7c77d616f819b2555e540a8764769e3a5061be4e
doveva <doveva@TeamCity>
2024-02-03 14:34:20 +0000

    TeamCity change in 'TestJava' project: runners of 'Deploy Test' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,20 +1,75 @@
-package TestJava.buildTypes
-
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.DockerCommandStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 
-object TestJava_DeployTest : BuildType({
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2023.11"
+
+project {
+    description = "Test Java pipeline"
+
+    buildType(DeployTest)
+
+    template(Docker)
+}
+
+object DeployTest : BuildType({
     name = "Deploy Test"
     description = "First Deploy for java project"
 
     vcs {
-        root(TestJava.vcsRoots.TestJava_GitHub)
+        root(DslContext.settingsRoot)
     }
 
     steps {
         gradle {
-            name = "Gradle build"
             id = "gradle_runner"
-            tasks = "bootJar"
+            tasks = "clean build"
+        }
+    }
+})
+
+object Docker : Template({
+    name = "Docker"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dockerCommand {
+            name = "Build"
+            id = "Build"
+            commandType = build {
+                source = file {
+                    path = "Dockerfile"
+                }
+                platform = DockerCommandStep.ImagePlatform.Linux
+                namesAndTags = "springtest:latest"
+                commandArgs = "--pull"
+            }
         }
     }
 })
